215ee2ad3165fee41a7a15f295c6f02d30be47cc
Jaiquez Hagood <west00brook@gmail.com>
2026-09-25 18:23:19 -0400

    attempt to fix error during build. Needed to wrap the useSearchParams in Suspense

--- app/components/GlobalMessageReader.tsx
+++ app/components/GlobalMessageReader.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import Notifier, { Severity } from './ui/snackbar';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
@@ -31,12 +31,14 @@ const GlobalMessageReader = () => {
   }, [searchParams, router]);
 
   return (
-    <Notifier
-      severity={Severity.error}
-      message={decodeURIComponent(searchParams.get('msg') || '')}
-      onClose={handleClose}
-      open={open}
-    />
+    <Suspense fallback={null}>
+      <Notifier
+        severity={Severity.error}
+        message={decodeURIComponent(searchParams.get('msg') || '')}
+        onClose={handleClose}
+        open={open}
+      />
+    </Suspense>
   );
 };
 
